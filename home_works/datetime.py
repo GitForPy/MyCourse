5034ce7bbdaaa4f2e94c0a5207c904614b916af4
Your Name <alexanderk9@mail.ru>
2020-05-17 17:33:35 +0300

    added solution

--- home_works/datetime.py
+++ home_works/datetime.py
@@ -5,3 +5,18 @@ from datetime import datetime
 На основе этой информации выводить пользователю сообщение:
 Добрый вечер/день/ночи в зависимости времени суток
 '''
+
+now = datetime.now()
+print(now)
+
+cur_hour = now.hour
+print(cur_hour)
+
+if 6 <= cur_hour <= 12:
+    print('утро')
+elif 12 < cur_hour <= 18:
+    print('день')
+elif 18 < cur_hour <= 24:
+    print('вечер')
+else:
+    print('ночь')
